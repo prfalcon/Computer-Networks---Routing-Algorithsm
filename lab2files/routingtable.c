48420aa530cf93f6ea2b84b508b3bb387810b61e
Vishnu Gopal <34254396+prfalcon@users.noreply.github.com>
2018-10-28 02:34:46 -0400

    Add files via upload

--- lab2files/routingtable.c
+++ lab2files/routingtable.c
@@ -4,22 +4,15 @@
 struct route_entry routingTable[MAX_ROUTERS];
 int NumRoutes;
 
-/* Routine Name    : InitRoutingTbl
- * INPUT ARGUMENTS : 1. (struct pkt_INIT_RESPONSE *) - The INIT_RESPONSE from Network Emulator
- *                   2. int - My router's id received from command line argument.
- * RETURN VALUE    : void
- * USAGE           : This routine is called after receiving the INIT_RESPONSE message from the Network Emulator. 
- *                   It initializes the routing table with the bootstrap neighbor information in INIT_RESPONSE.  
- *                   Also sets up a route to itself (self-route) with next_hop as itself and cost as 0.
- */
+
 void InitRoutingTbl(struct pkt_INIT_RESPONSE * InitResponse, int myID) {
 
     int i;
     struct nbr_cost costTemp;
 
     for (i = 0; i < InitResponse -> no_nbr; i++) {
-
-        costTemp = InitResponse -> nbrcost[i];
+		
+		costTemp = InitResponse -> nbrcost[i];
         routingTable[i].cost = costTemp.cost;
         routingTable[i].next_hop = costTemp.nbr;
         routingTable[i].dest_id = costTemp.nbr;
@@ -30,28 +23,13 @@ void InitRoutingTbl(struct pkt_INIT_RESPONSE * InitResponse, int myID) {
     routingTable[i].cost = 0;
 
     NumRoutes = i + 1;
-
 }
 
-/* Routine Name    : UpdateRoutes
- * INPUT ARGUMENTS : 1. (struct pkt_RT_UPDATE *) - The Route Update message from one of the neighbors of the router.
- *                   2. int - The direct cost to the neighbor who sent the update. 
- *                   3. int - My router's id received from command line argument.
- * RETURN VALUE    : int - Return 1 : if the routing table has changed on running the function.
- *                         Return 0 : Otherwise.
- * USAGE           : This routine is called after receiving the route update from any neighbor. 
- *                   The routing table is then updated after running the distance vector protocol. 
- *                   It installs any new route received, that is previously unknown. For known routes, 
- *                   it finds the shortest path using current cost and received cost. 
- *                   It also implements the forced update and split horizon rules. My router's id
- *                   that is passed as argument may be useful in applying split horizon rule.
- */
 int UpdateRoutes(struct pkt_RT_UPDATE * RecvdUpdatePacket, int costToNbr, int myID) {
 
     struct route_entry * tableEntry;
     struct route_entry updateEntry;
-
-    int i;
+	int i;
     int j;
     int hasChanged = 0;
     int entryFound;
@@ -60,15 +38,13 @@ int UpdateRoutes(struct pkt_RT_UPDATE * RecvdUpdatePacket, int costToNbr, int my
         entryFound = 0;
         updateEntry = RecvdUpdatePacket -> route[i];
         updateCost = updateEntry.cost + costToNbr;
-
-        for (j = 0; j < NumRoutes; j++) {
+		
+		for (j = 0; j < NumRoutes; j++) {
             tableEntry = & routingTable[j];
 
             if (tableEntry -> dest_id == updateEntry.dest_id) {
             	entryFound = 1;
-
-                if (updateCost < routingTable[j].cost && updateEntry.next_hop != myID){	
-					//If new cost is smaller and neighbor's next hop is not me
+				if (updateCost < routingTable[j].cost && updateEntry.next_hop != myID){	
 					routingTable[j].next_hop = RecvdUpdatePacket -> sender_id;
 					routingTable[j].cost = updateCost;
 					hasChanged = 1;
@@ -78,7 +54,6 @@ int UpdateRoutes(struct pkt_RT_UPDATE * RecvdUpdatePacket, int costToNbr, int my
 				}
 				break;	
             }
-
         }
 
         if (entryFound == 0) {
@@ -87,9 +62,42 @@ int UpdateRoutes(struct pkt_RT_UPDATE * RecvdUpdatePacket, int costToNbr, int my
             routingTable[NumRoutes].next_hop = RecvdUpdatePacket -> sender_id;
             routingTable[NumRoutes].cost = updateEntry.cost + costToNbr;
             hasChanged = 1;
-
         }
     }
 
     return hasChanged;
 }
+
+/* Routine Name    : ConvertTabletoPkt
+ * INPUT ARGUMENTS : 1. (struct pkt_RT_UPDATE *) - An empty pkt_RT_UPDATE structure
+ *                   2. int - My router's id received from command line argument.
+ * RETURN VALUE    : void
+ * USAGE           : This routine fills the routing table into the empty struct pkt_RT_UPDATE. 
+ *                   My router's id  is copied to the sender_id in pkt_RT_UPDATE. 
+ *                   Note that the dest_id is not filled in this function. When this update message 
+ *                   is sent to all neighbors of the router, the dest_id is filled.
+ */
+void ConvertTabletoPkt(struct pkt_RT_UPDATE *UpdatePacketToSend, int myID);
+
+
+
+/* Routine Name    : PrintRoutes
+ * INPUT ARGUMENTS : 1. (FILE *) - Pointer to the log file created in router.c, with a filename that uses MyRouter's id.
+ *                   2. int - My router's id received from command line argument.
+ * RETURN VALUE    : void
+ * USAGE           : This routine prints the routing table to the log file 
+ *                   according to the format and rules specified in the Handout.
+ */
+void PrintRoutes (FILE* Logfile, int myID);
+
+
+
+/* Routine Name    : UninstallRoutesOnNbrDeath
+ * INPUT ARGUMENTS : 1. int - The id of the inactive neighbor 
+ *                   (one who didn't send Route Update for FAILURE_DETECTION seconds).
+ *                   
+ * RETURN VALUE    : void
+ * USAGE           : This function is invoked when a nbr is found to be dead. The function checks all routes that
+ *                   use this nbr as next hop, and changes the cost to INFINITY.
+ */
+void UninstallRoutesOnNbrDeath(int DeadNbr);
